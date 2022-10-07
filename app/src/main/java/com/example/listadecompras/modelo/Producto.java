2b2a11ea441840e3d1b3d9505ec8116f01f66a83
christopher <topperapablaza@gmail.com>
2022-10-06 22:04:07 -0300

    ejemplo lista de compras arraylist

---
+++ app/src/main/java/com/example/listadecompras/modelo/Producto.java
@@ -0,0 +1,75 @@
+package com.example.listadecompras.modelo;
+
+import androidx.annotation.NonNull;
+
+public class Producto {
+    private  String nombre;
+    private  int cantidad;
+    private String unidad;
+    private boolean estado;
+
+    public static final boolean PENDIENTE=true;  // <-- variable constante
+    public static final boolean COMPRADO=false;  // <-- variable constante
+
+
+    /*
+    public static final Producto[] productos=
+            {
+                    new Producto("Bebida",1,"litro"),
+                    new Producto("Papas fritas",2,"Bolsas"),
+                    new Producto("Aceitunas",2,"paquetes")
+            };
+   */
+
+
+    public Producto(String nombre, int cantidad, String unidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.unidad = unidad;
+        this.estado = PENDIENTE;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setUnidad(String unidad) {
+        this.unidad = unidad;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public String getUnidad() {
+        return unidad;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    @Override
+    public String toString() {
+        String comprado;
+        if(estado==COMPRADO){
+            comprado="comprado";
+        }
+        else{
+            comprado="pendiente";
+        }
+        return nombre +": " + comprado;
+    }
+}
